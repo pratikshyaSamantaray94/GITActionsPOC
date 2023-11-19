a06e8be0baa48ea5e8187123eccbd644d7f2edd4
Pratikshya Samantaray <PSamantaray@liventus.com>
2023-11-19 23:38:25 +0530

    changes to check the cicd 1

--- src/test/java/GitHub/ActionsPOC/ActionsPOCTest.java
+++ src/test/java/GitHub/ActionsPOC/ActionsPOCTest.java
@@ -15,7 +15,7 @@ public class ActionsPOCTest {
 		driver.get("https://www.google.com");
 		driver.manage().window().maximize();
 		Thread.sleep(5000);
-		System.out.println("Added one commit for run2");
+		System.out.println("Added one commit for run3");
 		driver.close();
   }
 }
