e68f00a4b0aa9e43a5f94c89c4b2674bec42da5c
Pratikshya Samantaray <PSamantaray@liventus.com>
2023-11-20 00:48:39 +0530

    change to test class 12345

--- src/test/java/GitHub/ActionsPOC/ActionsPOCTest.java
+++ src/test/java/GitHub/ActionsPOC/ActionsPOCTest.java
@@ -13,7 +13,7 @@ public class ActionsPOCTest {
   public void ActionsPOC() throws InterruptedException {
 	    //System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
 	  	ChromeOptions co = new ChromeOptions();
-	  	co.setBrowserVersion("118");
+	  	co.setBrowserVersion("119");
 		driver = new ChromeDriver(co);
 		driver.get("https://www.google.com");
 		driver.manage().window().maximize();
