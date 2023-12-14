22d3b43d786515cb39aa9e0d97b3c95c9af01973
Sulagna304 <132049925+Sulagna304@users.noreply.github.com>
2023-12-14 20:21:44 +0530

    Update ActionsPOCTest.java

--- src/test/java/GitHub/ActionsPOC/ActionsPOCTest.java
+++ src/test/java/GitHub/ActionsPOC/ActionsPOCTest.java
@@ -4,23 +4,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ActionsPOCTest {
 	
   static WebDriver driver;
 	
+	/*
+	 * @Test public void ActionsPOC() throws InterruptedException {
+	 * //System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	 * WebDriverManager.chromedriver().setup(); ChromeOptions co = new
+	 * ChromeOptions(); //co.setBrowserVersion("118"); driver = new
+	 * ChromeDriver(co); driver.get("https://www.google.com");
+	 * driver.manage().window().maximize(); Thread.sleep(5000);
+	 * System.out.println("Added one commit for run");
+	 * 
+	 * }
+	 */
+
   @Test
-  public void ActionsPOC() throws InterruptedException {
-	    //System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-	  	WebDriverManager.chromedriver().setup();
-	  	ChromeOptions co = new ChromeOptions();
-	  	//co.setBrowserVersion("118");
-		driver = new ChromeDriver(co);
-		driver.get("https://www.google.com");
-		driver.manage().window().maximize();
-		Thread.sleep(5000);
-		System.out.println("Added one commit for run");
-		driver.close();
+  public void NewTestActionsPOC() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		String baseUrl = "https://www.google.com";
+        System.out.println("Launching Google Chrome browser"); 
+        driver = new ChromeDriver();
+        driver.get(baseUrl);
+        String testTitle = "Free QA Automation Tools For Everyone";
+        String originalTitle = driver.getTitle();
+        driver.close();
   }
 }
