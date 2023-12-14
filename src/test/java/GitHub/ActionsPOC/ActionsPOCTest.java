098f3133be165f97596bdad7b9bb947efc4b88ed
Sulagna304 <132049925+Sulagna304@users.noreply.github.com>
2023-12-14 20:07:39 +0530

    Update ActionsPOCTest.java

--- src/test/java/GitHub/ActionsPOC/ActionsPOCTest.java
+++ src/test/java/GitHub/ActionsPOC/ActionsPOCTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ActionsPOCTest {
 	
