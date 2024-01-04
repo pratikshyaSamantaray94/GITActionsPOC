5cc81ad12570cc43d997fd53badc7596078dcc71
Pratikshya Samantaray <PSamantaray@liventus.com>
2024-01-04 18:59:38 +0530

    latest change

--- src/test/java/GitHub/ActionsPOC/ActionsPOCTest.java
+++ src/test/java/GitHub/ActionsPOC/ActionsPOCTest.java
@@ -11,44 +11,11 @@ public class ActionsPOCTest {
 	
   static WebDriver driver;
 	
-	/*
-	 * @Test public void ActionsPOC() throws InterruptedException {
-	 * //System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-	 * WebDriverManager.chromedriver().setup(); ChromeOptions co = new
-	 * ChromeOptions(); //co.setBrowserVersion("118"); driver = new
-	 * ChromeDriver(co); driver.get("https://www.google.com");
-	 * driver.manage().window().maximize(); Thread.sleep(5000);
-	 * System.out.println("Added one commit for run");
-	 * 
-	 * }
-	 */
 
   @Test
   public void MethodForGitHubAction() throws InterruptedException {
-		/*WebDriverManager.chromedriver().setup();
-		String baseUrl = "https://www.google.com";
-        System.out.println("Launching Google Chrome browser"); 
-        driver = new ChromeDriver();
-        driver.get(baseUrl);
-        String testTitle = "Free QA Automation Tools For Everyone";
-        String originalTitle = driver.getTitle();
-        driver.close();*/
-	    String baseUrl = "https://www.google.com";
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--disable-dev-shm-usage");
-//        options.addArguments("--ignore-ssl-errors=yes");
-//        options.addArguments("--ignore-certificate-errors");
-//        options.setBrowserVersion("119");
-        
+	    String baseUrl = "https://www.google.com";        
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("start-maximized"); // open Browser in maximized mode
-//        options.addArguments("disable-infobars"); // disabling infobars
-//        options.addArguments("--disable-extensions"); // disabling extensions
-//        options.addArguments("--disable-gpu"); // applicable to windows os only
-//        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-//        options.addArguments("--no-sandbox"); // Bypass OS security model
-//        options.addArguments("--remote-debugging-port=9222");       
-//        WebDriver driver = new ChromeDriver(options);
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get(baseUrl);
