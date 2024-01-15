cc9ae1fab29bbe6d7612cd36339df0a21d00ff70
Pratikshya Samantaray <PSamantaray@liventus.com>
2024-01-15 20:17:54 +0530

    added proxy for chrome

--- src/test/java/GitHub/ActionsPOC/ActionsPOCTest.java
+++ src/test/java/GitHub/ActionsPOC/ActionsPOCTest.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.time.Duration;
 
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -20,17 +21,22 @@ public class ActionsPOCTest {
 
   @Test
   public void MethodForGitHubAction() throws InterruptedException, MalformedURLException {
-	    String baseUrl = "https://www.google.com";        
+	    String baseUrl = "https://www.google.com";   
+	    
+	    Proxy proxy = new Proxy();
+	    proxy.setHttpProxy("<HOST:PORT>");
+	    
         ChromeOptions options = new ChromeOptions();
+        options.setCapability("proxy", proxy);
+//        options = webdriver.ChromeOptions();
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--ignore-ssl-errors=yes");
+//        options.addArguments("--ignore-certificate-errors");
+        
 //        options.addArguments("headless");
 //        options.setHeadless(true);
-        driver = new ChromeDriver(options);
         
-//        DesiredCapabilities cap = new DesiredCapabilities(); 
-//        ClientConfig config = ClientConfig.defaultConfig().connectionTimeout(Duration.ofMinutes(20))
-//        		.readTimeout(Duration.ofMinutes(20)); // I change this 3 minute(Default) to 20 minutes.
-//        cap.setPlatform(Platform.WIN10); cap.setBrowserName("chrome"); 
-//        WebDriver driver = new RemoteWebDriver(new URL("https://www.google.com/wd/hub"), options);
+        driver = new ChromeDriver(options);
         
         driver.get(baseUrl);
         String originalTitle = driver.getTitle();
